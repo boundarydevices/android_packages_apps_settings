cbf90c965944c8576f83668a85fe01f3087001ef
Gary Bisson <gary.bisson@boundarydevices.com>
2016-03-24 15:36:12 +0100

    DeviceInfoSettings: modify build number field
    In order for it to be more detailed, it now displays:
    - type of build (eng or user)
    - android DISPLAY version
    - android BUILD_NUMBER
    - security keys used
    - build date
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -97,7 +97,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
-        setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
+        setStringSummary(KEY_BUILD_NUMBER, SystemProperties.get("ro.build.description") +
+			"\n" + SystemProperties.get("ro.build.date"));
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
 
